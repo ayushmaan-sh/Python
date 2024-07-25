6659fbb486a117f9abbaafcff5f8029e7b09b433
Ayushmaan Shukla <102596631+ayushmaan-sh@users.noreply.github.com>
2024-07-25 23:55:16 +0530

    Add weather_situation.py

---
+++ 02_conditionals/weather_situation.py
@@ -0,0 +1,8 @@
+weather_condition = "Snowy"
+
+if weather_condition == "Sunny":
+    print("Go for a walk")
+elif weather_condition == "Rainy":
+    print("Read a book")
+elif weather_condition == "Snowy":
+    print("Build a snowman")
