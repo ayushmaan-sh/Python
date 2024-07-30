da584c3cf37e144a321f6e23b44b29d66c340a8e
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2024-07-30 23:19:48 +0530

    Function that multiply numbers & string.

---
+++ 04_functions/polymorphism.py
@@ -0,0 +1,7 @@
+# Write a function that multiplies two numbers, but can also accept and multiply strings.
+
+def multiply(value_1, value_2):
+    return value_1*value_2
+
+print(multiply(7,7))  # multiplying numbers.
+print(multiply("a",7))  # multiplying numbers with strings.
