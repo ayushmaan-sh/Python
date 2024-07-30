094ad3749e99ecca6ce880ad8a42cb4a5881f8cf
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2024-07-30 23:57:45 +0530

    Functions which has no name.

---
+++ 04_functions/lambda_function.py
@@ -0,0 +1,6 @@
+# Write a lambda function to compute the cube of a number
+#  Lambda function are those function which has no name
+
+cube = lambda number: number ** 3  # lambda functions are mainly used in frameworks like django, flask and even in some libraries also.
+
+print(cube(3))
