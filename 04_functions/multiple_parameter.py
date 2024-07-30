19eb615488cf03e66460ecf9be437bccd38ff895
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2024-07-30 23:10:28 +0530

    Function that takes 2 parameters and add it

---
+++ 04_functions/multiple_parameter.py
@@ -0,0 +1,7 @@
+# Create a funtion that takes two numbers as parameters and returns their sum
+
+def add_two_number(number_1, number_2):
+    return number_1 + number_2
+
+sum = add_two_number(5, 8)
+print(sum)
