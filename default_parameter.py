b2683967167d4b7020e51ca2c4b00ba4616ad10d
Ayushmaan Shukla <ayushmaanshukla274@gmail.com>
2024-07-30 23:56:39 +0530

    Delete default_parameter.py

--- default_parameter.py
+++
@@ -1,6 +0,0 @@
-# Write a function that greets user with it's name. If no user input, greet a default name.
-
-def greet(name = "User"):  #defined a default value
-    return "Hello, "+name+"!"
-
-print(greet())
